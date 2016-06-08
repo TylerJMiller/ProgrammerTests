1450765e61197ed5486d5a5518ac679624ff77df
tyler <tyler.miller@students.aie.edu.au>
2016-06-08 15:49:46 -0700

    Finished first question

--- ProgramTests/GearboxTest/main.cpp
+++ ProgramTests/GearboxTest/main.cpp
@@ -1,4 +1,34 @@
-int main()
+#include <iostream>
+using namespace std;
+
+
+int MultiplyBy123(int value)
 {
+	int rslt;
+
+	//123
+	//64 32 16 8 2 1
+	//6  5  4  3 1 0
+	rslt = (value<<6) + (value<<5) + (value<<4) + (value<<3) + (value<<1) + value;
+
+	return rslt;
+}
+
+void SortArray(int* a, int length)
+{
+
+}
+
+int Main()
+{
+	cout << MultiplyBy123(10) << "\n";
+	char input[10];
+
+	const int length = 20;
+	int a[length];
+
+
+	cin >> input;
 	return 0;
 }
+
